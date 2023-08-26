fe0ce852b9d83a9ffed19457b4bead06fe37ce3f
Kate Zaharova <15071981ktz@gmail.com>
2023-08-26 19:35:29 +0300

    FeedbackBtn.styled added

---
+++ src/components/FeedbackBtn/FeedbackBtns.js
@@ -0,0 +1,20 @@
+import {Button} from "./FeedbackBtn.styled";
+
+
+export const FeedbackBtns = () => {
+  return (
+    <div>
+      <h2>FeedbackBtns</h2>
+      <Button>Good</Button>
+      <button>Neutral</button>
+      <button>Bad</button>
+
+      <h2>Statistics</h2>
+      <p>Neutral: </p>
+      <p>Good: </p>
+
+
+    </div>
+    
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { FeedbackBtns } from 'components/FeedbackBtn/FeedbackBtns';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <FeedbackBtns/>
   </React.StrictMode>
 );
